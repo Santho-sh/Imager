097f1b073a96cb839c66bc22ae7e6ac39264bb43
Santhosh <santhoshkalish333@gmail.com>
2023-01-27 01:31:06 +0530

    greycale working others none

--- filter.py
+++ filter.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageFilter
 from sys import exit
 from colorama import Fore
 import re
+import cv2
 
 
 def main():
@@ -38,7 +39,7 @@ def validate_image(name):
 
 def get_filter():
     # get which filter to use
-    print("Enter which filter you want to apply? \n[1] Greyscale \n[2] Blur \n[3] Sharpen \n[4] Reverse \n[5] Rotate Upside Down \n[6] Resize the Image")
+    print("Enter which filter you want to apply? \n[1] Greyscale \n[2] Blur \n[3] Sharpen \n[4] Reverse \n[5] Rotate \n[6] Resize the Image\n")
     while True:
         filter = input("Filter No: ")
         
@@ -47,42 +48,60 @@ def get_filter():
                 return "greyscale"
             case "2":
                 return "blur"
-            case "2":
-                return "sharpen"
             case "3":
-                return "reverse"
+                return "sharpen"
             case "4":
-                return "rotale"
+                return "reverse"
             case "5":
+                return "rotale"
+            case "6":
                 return "resize"
             case _:
-                print("Please Enter the filter [1 to 5] you want...")
+                print("Please Enter the filter [1 to 5]")
                 continue
-    
 
 def apply_filter(image_name, filter_name):
     try:
-        image = Image.open(image_name)
+          
+        # Greyscale
+        if filter_name == "greyscale":
+            image = cv2.imread(image_name)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite(f"edited_{image_name}", image)
+            
+        # Sharpen
+        elif  filter_name == "sharpen":
+            image = Image.open(image_name)
+            image = image.filter(ImageFilter.SHARPEN)
+            image = image.save(f"edited_{image_name}")
+            
+        # Reverse
+        elif filter_name == "reverse":
+            ...
+        
+        # chanege file format
+        elif filter_name == "change":
+            ...
+        
+        # blur image
+        elif filter_name == "blur":
+            image = Image.open(image_name)
+            image = image.filter(ImageFilter.BLUR)
+            image = image.save(f"edited_{image_name}")
+        
+        # Rotate
+        elif filter_name == "rotate":
+            ...
+            
+        # resize
+        else:
+            None
         
     except FileNotFoundError:
         exit(Fore.RED  + "File doesn't exit,\nImage must be in same directory" + Fore.WHITE)
-    
-    # Greyscale
-    # Sharpen
-    if  filter_name == "sharpen":
-        image = image.filter(ImageFilter.SHARPEN)
-        image = image.save(f"edited_{image_name}")
-    # Reverse
-    # chanege file format
-    # blur image
-    elif filter_name == "blur":
-        image = image.filter(ImageFilter.BLUR)
-        image = image.save(f"edited_{image_name}")
-    # Rotate
-    # resize
-    else:
-        None
-
+        
+    # save the image
+    print(Fore.GREEN + "Image Saved Successfully" + Fore.WHITE)
 
 
 if __name__ == "__main__":
